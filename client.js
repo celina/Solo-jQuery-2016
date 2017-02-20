faf63166d2a3c636b0487517785627b9fe0e30b1
Celina Peterson <celinap@gmail.com>
2017-02-20 14:30:29 -0600

    final commit

--- client.js
+++ client.js
@@ -23,7 +23,42 @@ function buildColorCube() {
     console.log('Box ' + $(this).data().color + ' was clicked!'); // this calls out a property on the object
 
     var newDiv = $('<div class="color-cube" data-color="' + $(this).data().color + '" style="background-color: ' + $(this).data().color + '"></div>');
-    newDiv.text('Test');
+    newDiv.text();
     $('.colorcontainer').append(newDiv);
-  });
+    $('#green').text('1');
+    $(this).data().color
+    $(span).id($(this).data().color).text('1');
+
+
+
+//     div = $( "div" )[ 0 ];
+// switch ( $( "button" ).index( this )
+
+    // $(this).data().color$
+    // $('span').attr($(this).data().color).text('test');
+    // $('span').id($(this).data().color).text('test');
+    // $('span').id('green').text('test');
+    // $('green').text('0');
+    // $('span').find('id').attr('green').replace('1');
+
+    // ​$('span').id().text('1');
+  //     if ($(this).data('count')) { // already been clicked
+  //         $(this).data('count', $(this).data('count') + 1); // add one
+  //     } else { // first click
+  //         $(this).data('count', 1); // initialize the count
+  //     }
+  //     $(this).html($(this).data('count')); // show it
+});
 }
+
+
+// $function numColors() {
+// 	$('.container').click(function(){
+//         if ($(this).data('count')) { // already been clicked
+//             $(this).data('count', $(this).data('count') + 1); // add one
+//         } else { // first click
+//             $(this).data('count', 1); // initialize the count
+//         }
+//         $(this).html($(this).data('count')); // show it
+//     });
+// });
