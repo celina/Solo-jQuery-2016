7d76baa5a250e2cbd63e71e5a2eb3d689fd31076
Celina Peterson <celinap@gmail.com>
2017-02-20 13:47:56 -0600

    add colorcube on button push

--- client.js
+++ client.js
@@ -1,4 +1,29 @@
+var colorsArray = ['Red', 'Yellow', 'Green', 'Blue'];
+
+function Colors(name) {
+  this.name = name;
+}
+
 $(document).ready(function() {
   console.log('Ready!');
-  
+
+  init();
+  buildColorCube();
+
 });
+
+function init() {
+
+}
+
+function buildColorCube() {
+  $('button').on('click', function() {
+    var pickedColor = $(this).data().color;
+
+    console.log('Box ' + $(this).data().color + ' was clicked!'); // this calls out a property on the object
+
+    var newDiv = $('<div class="color-cube" data-color="' + $(this).data().color + '" style="background-color: ' + $(this).data().color + '"></div>');
+    newDiv.text('Test');
+    $('.colorcontainer').append(newDiv);
+  });
+}
